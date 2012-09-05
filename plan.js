9e0f8d86ad2711aa11f223234b883622c290892f
John Bond <john.r.bond@gmail.com>
2012-09-05 22:47:19 +0200

    alpha working code

--- plan.js
+++ plan.js
@@ -2,41 +2,42 @@
 //@arg atteckers array containing coordinates to defending vills
 //@return 2D associative array where
 // array[attack coordinates][defender coordinates] = distance
-function get_distances(attackers, defenders) {
-	var distances = new Array()
+function get_travel_times(attackers, defenders, speed) {
+	//$travelTime = $distance*$speed*60;
+	var travel_times = new Array()
 	for ( var a_count = 0; a_count < attackers.length; a_count++) {
 		attacker = attackers[a_count].split("|");
-		distances[attackers[a_count]] = new Array()
+		travel_times[attackers[a_count]] = new Array()
 		//a big number 
 		for ( var d_count = 0; d_count < defenders.length; d_count++) {
 			defender = defenders[d_count].split("|");
 			x = attacker[0] - defender[0];
 			y = attacker[1] - defender[1];
 			distance = Math.sqrt((x * x) + (y * y));
-			distances[attackers[a_count]][defenders[d_count]] = distance;
+			travel_times[attackers[a_count]][defenders[d_count]] = distance * speed * 60;
 		}
 	}
-	return distances	
+	return travel_times	
 }
 //@arg distances 2D associative array where
 // array[attack coordinates][defender coordinates] = distance
-function get_plan(distances, max_attack) {
+function get_plan(travel_times, max_attack) {
 	var plan = new Array();
 	var used_targets = new Array();
-	for ( attack in distances) {
+	for ( attack in travel_times) {
 		var fastest = 9999999999.0;
 		var target = "";
-		var distance = "";
+		var travel_time = "";
 		plan[attack] = new Array();
-		for ( defend in distances[attack]) {
+		for ( defend in travel_times[attack]) {
 			if (typeof used_targets[defend] === 'undefined'  ) {
 				used_targets[defend] = 0; 
 			} 
 			if (  used_targets[defend] < max_attack) {
-				if ( distances[attack][defend] < fastest ) {
+				if ( travel_times[attack][defend] < fastest ) {
 					target = defend;
-					distance = distances[attack][defend];
-					fastest = distance;
+					travel_time = travel_times[attack][defend];
+					fastest = travel_time;
 				}
 			}
 		}
@@ -45,10 +46,22 @@ function get_plan(distances, max_attack) {
 			//console.log(target);
 			//console.log(distance);
 			plan[attack]['target'] = target;
-			plan[attack]['speed'] = distance;
+			plan[attack]['travel_time'] = travel_time;
 		}
 	}
 	return plan	
+}
+
+function get_results(plan, land_time) {
+	var result = "";
+	for (attack in plan) {
+		console.log(plan[attack]['target'] );
+		if (plan[attack]['target'] != undefined || plan[attack]['travel_time'] != undefined) {
+			var launch_time = new Date(Math.floor(Date.parse(land_time) - (plan[attack]['travel_time']*1000)));
+			result += attack + " -> " +  plan[attack]['target'] + " @ " + launch_time.toString('dd/mm/yyyy HH:mm:ss') + "<br />";
+		}
+	}
+	return result;
 }
 	
 $(function() {  
@@ -64,21 +77,31 @@ $(function() {
 		var clearing_coords = $("textarea#clearing_coords").val().match(coord_regex);
 		var support_coords = $("textarea#support_coords").val().match(coord_regex);
 		var targets_coords = $("textarea#target_coords").val().match(coord_regex);
+		var clearing_count = $("input#clearing_count").val()
+		var support_count = $("input#support_count").val()
+		var noble_count = $("input#noble_count").val()
+		//var clearing_results = $("div#clearing_results")
+		//var support_results = $("div#support_results")
+		//var targets_results = $("div#target_results")
 		if ( clearing_coords ) { 
-			var clearing_distances = get_distances(clearing_coords,targets_coords);
+			var clearing_travel_times = get_travel_times(clearing_coords,targets_coords,clearing_speed);
 			//console.log(clearing_distances);
-			var clearing_plan = get_plan( clearing_distances,3);
-			console.log(clearing_plan);
+			var clearing_plan = get_plan( clearing_travel_times,clearing_count);
+			//console.log(clearing_plan);
+			$("div#clearing_results").html(get_results(clearing_plan,arrival_time));
+			
 		}
 		if ( noble_coords ) { 
-			var noble_distances = get_distances(noble_coords,targets_coords);
+			var noble_travel_times = get_travel_times(noble_coords,targets_coords,noble_speed);
 			//console.log(noble_distance);
-			var noble_plan = get_plan( noble_distances,3);
-			console.log(noble_plan);
+			var noble_plan = get_plan( noble_travel_times,noble_count);
+			//console.log(noble_plan);
+			$("div#noble_results").html(get_results(noble_plan,arrival_time));
 		}
 		if ( support_coords ) { 
-			var support_distances = get_distances(support_coords,targets_coords);
-			var support_plan = get_plan( support_distances,3);
+			var support_travel_times = get_travel_times(support_coords,targets_coords,support_speed);
+			var support_plan = get_plan( support_travel_times,support_count);
+			$("div#support_results").html(get_results(support_plan,arrival_time));
 		}
 	});  
 });  
